dc351eaa405c6338fe046731e2244c2842b3ff79
Sf-Git-2023 <sf.chang232tpv@gmail.com>
2023-10-03 11:52:35 +0800

    update 1003-11:52

---
+++ index.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "<h1>Hello, Flask!</h1><h2>Flask2級<h2><p>這是段落內容1111</p>"
+
+@app.route("/hello_world")
+def hello_world():
+    return "<h1>Hello! World.</h1>"
